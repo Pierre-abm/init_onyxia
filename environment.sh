c21c0ad98951c116abf71cae8ce0779cd1c60679
Pierre-abm <pierre.abraham.etu@gmail.com>
2026-03-04 11:30:57 +0100

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,5 +1,5 @@
 GIT_REPO=detection_cypres
-git clone --depth 1 git clone https://oauth2:ShSEnZ3w1vbK5vM8Z2gamG86MQp1Ojlhegk.01.0z0ihe40f@forge.inrae.fr:dynafor/dev-test/pierre-abraham/detection_cypres.git
+git clone --depth 1 git clone https://pierre.abraham:ShSEnZ3w1vbK5vM8Z2gamG86MQp1Ojlhegk.01.0z0ihe40f@forge.inrae.fr/dynafor/dev-test/pierre-abraham/detection_cypres.git
 chown -R onyxia:users ${GIT_REPO}/
 
 pip install -r ${GIT_REPO}/requirements.txt
